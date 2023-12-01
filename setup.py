b6d57414811f3d3fda7c199c96acadb84b6187d5
m3trik@live.com <m3trik@live.com>
2023-12-01 14:49:50 -0500

    updated: requirements.txt

--- setup.py
+++ setup.py
@@ -12,6 +12,11 @@ description = ptk.get_text_between_delimiters(
     as_string=True,
 )
 
+# Read requirements.txt and add to install_requires
+with open("requirements.txt") as f:
+    required_packages = f.read().splitlines()
+
+
 setuptools.setup(
     name=__package__,
     version=__version__,
@@ -23,13 +28,14 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/m3trik/{__package__}",
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
         "Operating System :: OS Independent",
     ],
+    packages=setuptools.find_packages(),
     include_package_data=True,
+    install_requires=required_packages,
     data_files=ptk.get_dir_contents(
         "map_compositor", "filepath", exc_files=["*.py", "*.pyc", "*.json"]
     ),  # ie. ('uitk/ui/0', ['uitk/ui/0/init.ui']),
