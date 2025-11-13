c19e2f675b87a7e57ac02102c43669f9fd6c6e7a
m3trik@live.com <m3trik@live.com>
2025-11-13 10:07:12 -0500

    Update __init__.py

--- map_compositor/__init__.py
+++ map_compositor/__init__.py
@@ -1,5 +1,31 @@
 # !/usr/bin/python
 # coding=utf-8
+from pythontk.core_utils.module_resolver import bootstrap_package
 
 __package__ = "map_compositor"
 __version__ = "0.5.16"
+
+
+DEFAULT_INCLUDE = {
+    "_map_compositor": [
+        "MapCompositorSignals",
+        "MapCompositor",
+        "MapCompositorSlots",
+        "MapCompositorUI",
+    ]
+}
+
+
+bootstrap_package(
+    globals(),
+    include=DEFAULT_INCLUDE,
+)
+
+
+__all__ = [
+    "MapCompositorSignals",
+    "MapCompositor",
+    "MapCompositorSlots",
+    "MapCompositorUI",
+    "__version__",
+]
