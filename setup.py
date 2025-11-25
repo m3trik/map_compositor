47336c9bbb8e183d7baa332bfaf5235a323dfc25
m3trik@live.com <m3trik@live.com>
2025-11-25 14:50:22 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
     ],
     packages=setuptools.find_packages(),
     include_package_data=True,
-    install_requires=ptk.update_requirements(exc=["Pillow", "qtpy"]),
+    install_requires=ptk.PackageManager.update_requirements(exc=["Pillow", "qtpy"]),
     data_files=ptk.get_dir_contents(
         __package__,
         "filepath",
